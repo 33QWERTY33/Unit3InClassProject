c60af85cd6b756cce099fbadc6233dffdffecf52
33QWERTY33 <120336644+33QWERTY33@users.noreply.github.com>
2024-02-28 19:27:04 -0500

    copied in class handout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Buttons from './components/Buttons';
+import Forms from './components/Forms';
+import List from './components/List';
+import Modal from './components/Modal';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import './app.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/buttons" element={<Buttons />} />
+          <Route path="/forms" element={<Forms />} />
+          <Route path="/list" element={<List />} />
+          <Route path="/modal" element={<Modal />} />
+        </Routes>
+      </div>
+    </Router>
+  )
 }
 
 export default App;
